16265574dfc4b6bacfc86b9263d83daaab2afc42
ez <105545215+ezcolin2@users.noreply.github.com>
2024-06-06 22:09:47 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 189ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240606/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.js
+++ 240606/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.js
@@ -4,8 +4,9 @@ const [k, n] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 // 뽑은 숫자들
 const combination = [];
 
-// cnt는 지금까지 선택한 숫자 개수
+// cnt 자리에 숫자를 선택하는 하뭇
 function makeCombination(cnt){
+
     // n 개를 선택했을 때
     if (cnt == n){
         console.log(combination.join(' '));
@@ -14,9 +15,8 @@ function makeCombination(cnt){
     
     // 1이상 k이하 숫자를 선택
     for (let i=1;i<=k;i++){
-        // 세 개 연속 같은 숫자는 건너뜀.
-        if (i>=3 && combination[i-1] == combination[i-2] == combination[i-3]){
-            continue;            
+        if (cnt>=2 && i == combination[cnt-1] == combination[cnt-2]){
+            return;
         }
         combination.push(i);
         makeCombination(cnt+1);
