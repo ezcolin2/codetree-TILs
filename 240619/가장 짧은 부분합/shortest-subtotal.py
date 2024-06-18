1db4f1a8ef378752f95bcf72f3054c43505f1cbb
ez <105545215+ezcolin2@users.noreply.github.com>
2024-06-19 06:54:19 +0900

    Title: 가장 짧은 부분합, Time: 5076ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 240619/가장 짧은 부분합/shortest-subtotal.py
+++ 240619/가장 짧은 부분합/shortest-subtotal.py
@@ -9,6 +9,8 @@ res = 100000
 for i in range(n):
     sum_val = 0
     for j in range(i, n):
+        if j-i+1 >=res:
+            break
         sum_val += arr[j]
         # 처음으로 s가 넘으면 시작점이 i인 곳의 가장 짧은 길이를 구한 것이기 때문에 break
         if sum_val >= s:
