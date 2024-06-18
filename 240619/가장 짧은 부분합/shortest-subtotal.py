b845db0f331003f111082051a2bcbc232748b325
ez <105545215+ezcolin2@users.noreply.github.com>
2024-06-19 06:48:58 +0900

    Title: 가장 짧은 부분합, Time: 5048ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 240619/가장 짧은 부분합/shortest-subtotal.py
+++ 240619/가장 짧은 부분합/shortest-subtotal.py
@@ -5,10 +5,11 @@ arr = list(map(int, input().split()))
 res = 100000
 # two pointer
 for i in range(n):
+    sum_val = 0
     for j in range(i, n):
-        temp = sum(arr[i: j+1])
+        sum_val += arr[j]
         # 처음으로 s가 넘으면 시작점이 i인 곳의 가장 짧은 길이를 구한 것이기 때문에 break
-        if temp >= s:
+        if sum_val >= s:
             res = min(res, j-i+1)
             break
 print(res)
