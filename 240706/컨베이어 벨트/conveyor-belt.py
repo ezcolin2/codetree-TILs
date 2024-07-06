3bae6174dea9c155a06ee8864a0a29ae6025dccf
ez <105545215+ezcolin2@users.noreply.github.com>
2024-07-06 13:32:33 +0900

    Title: 컨베이어 벨트, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240706/컨베이어 벨트/conveyor-belt.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+n, t = map(int, input().split())
+arr = list(input().split()) + list(input().split())
+
+# 회전하는 함수
+# 뒤에서부터 한 칸씩 앞으로 땡긴다.
+def rotate(n):
+    for _ in range(n):
+        temp = arr[-1] # 마지막 값을 가져온다.
+        for i in range(len(arr)-1, 0, -1):
+            arr[i] = arr[i-1]
+        arr[0] = temp
+rotate(t)
+
+print(' '.join(arr[0:3]))
+print(' '.join(arr[3:6]))
