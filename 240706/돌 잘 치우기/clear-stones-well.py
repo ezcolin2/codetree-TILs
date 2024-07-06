a6da4f6daf713a1f1e8b691fde877df301f8f79d
ez <105545215+ezcolin2@users.noreply.github.com>
2024-07-06 20:20:53 +0900

    Title: 돌 잘 치우기, Time: 190ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240706/돌 잘 치우기/clear-stones-well.py
+++ 240706/돌 잘 치우기/clear-stones-well.py
@@ -34,15 +34,16 @@ def bfs(arr):
     while(len(queue)>0):
         # 큐에서 값을 빼온다.
         x, y = queue.popleft()
-
+        if can_go(x, y, visited):
+            visited[x][y] = True # 방문
+            visitedCnt += 1 
         for i in range(4):
             nx, ny = x+dx[i], y+dy[i]
             # 갈 수 있다면
             if can_go(nx, ny, visited):
-                print(nx, ny)
-                queue.append([nx, ny])
                 visited[nx][ny] = True # 방문
                 visitedCnt += 1
+                queue.append([nx, ny])
     return visitedCnt
 
 max_val = 0
