b5bfcdae502a207b1490095a4f55247a2f453f5c
ez <105545215+ezcolin2@users.noreply.github.com>
2024-07-11 06:39:10 +0900

    Title: 계단 오르기 2, Time: 127ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240711/계단 오르기 2/climbing-stairs-2.py
+++ 240711/계단 오르기 2/climbing-stairs-2.py
@@ -21,4 +21,4 @@ for i in range(1, n+1):
         if i -2 >= 0 and dp[i-2][j] != MIN_VAL:
             dp[i][j] = max(dp[i][j], dp[i-2][j] + stairs[i])
 
-print(max(dp[n]))
+print(max([max(row) for row in dp]))
