87c0bb8fe23b3ceae97c19523c246be2dcc526fa
ez <105545215+ezcolin2@users.noreply.github.com>
2024-08-20 09:53:24 +0900

    Title: 1차원 젠가, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240820/1차원 젠가/jenga-1d.py
+++ 240820/1차원 젠가/jenga-1d.py
@@ -5,7 +5,6 @@ arr = [int(input()) for _ in range(n)]
 
 s1, e1 = map(lambda x : int(x) - 1, input().split())
 s2, e2 = map(lambda x : int(x) - 1, input().split())
-arr.reverse()
 
 def eliminate(arr, s, e):
     new_arr = []
@@ -14,7 +13,6 @@ def eliminate(arr, s, e):
     for i in arr:
         if i != 0:
             new_arr.append(i)
-    new_arr.reverse()
     return new_arr
 arr = eliminate(arr, s1, e1)
 arr = eliminate(arr, s2, e2)
