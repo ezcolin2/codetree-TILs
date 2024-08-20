95633df54dd28084eedff077f538f26889fa13eb
ez <105545215+ezcolin2@users.noreply.github.com>
2024-08-20 09:48:54 +0900

    Title: 1차원 젠가, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240820/1차원 젠가/jenga-1d.py
+++ 240820/1차원 젠가/jenga-1d.py
@@ -3,23 +3,21 @@ input = sys.stdin.readline
 n = int(input())
 arr = [int(input()) for _ in range(n)]
 
-s1, e1 = map(int, input().split())
-s2, e2 = map(int, input().split())
+s1, e1 = map(lambda x : int(x) - 1, input().split())
+s2, e2 = map(lambda x : int(x) - 1, input().split())
+arr.reverse()
 
-# arr[s]에서 arr[e] 블록을 빼고 난 뒤 
 def eliminate(arr, s, e):
     new_arr = []
-    # 0으로 만듦
-    for i in range(len(arr) - s, len(arr) - e - 1, -1):
+    for i in range(s, e+1):
         arr[i] = 0
-    # 0이 아닌 경우만 new_arr에 추가
-    for i in arr[::-1]:
+    for i in arr:
         if i != 0:
             new_arr.append(i)
     return new_arr
-
 arr = eliminate(arr, s1, e1)
 arr = eliminate(arr, s2, e2)
+arr.reverse()
 print(len(arr))
 for i in arr:
     print(i)
