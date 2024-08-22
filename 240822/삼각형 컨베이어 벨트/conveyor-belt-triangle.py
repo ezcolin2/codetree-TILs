aa4c5cf05840b0eb49508770b44498086be7d9ed
ez <105545215+ezcolin2@users.noreply.github.com>
2024-08-22 09:38:26 +0900

    Title: 삼각형 컨베이어 벨트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240822/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
+++ 240822/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -2,13 +2,12 @@ import sys
 input = sys.stdin.readline
 n, t = map(int, input().split())
 arr = []
-for _ in range(n):
+for _ in range(3):
     arr += list(map(int, input().split()))
-
-new_arr = [0 for _ in range(n**2)]
+new_arr = [0 for _ in range(len(arr))]
 # 마지막 값 저장
 temp = arr[-1]
-for i in range(n**2 - 1, -1, -1):
+for i in range(n*3 - 1, -1, -1):
     new_arr[i] = arr[i-t]
 for i in new_arr[:n]:
     print(i, end = ' ')
