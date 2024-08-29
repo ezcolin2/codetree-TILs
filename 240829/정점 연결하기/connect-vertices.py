45329bb76a5db76f79ca0d7544c50a505520577f
ez <105545215+ezcolin2@users.noreply.github.com>
2024-08-29 16:16:47 +0900

    Title: 정점 연결하기, Time: 644ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240829/정점 연결하기/connect-vertices.py
+++ 240829/정점 연결하기/connect-vertices.py
@@ -3,7 +3,7 @@ input = sys.stdin.readline
 n = int(input())
 uf = [i for i in range(n+1)]
 arr = [list(map(int, input().split())) for _ in range(n-2)]
-arr.sort()
+groups = [[i] for i in range(n+1)]
 
 # 어떠한 그룹의 루트 노드 
 s = set()
@@ -19,7 +19,15 @@ for dots in arr:
     root_a = find_root(a)
     root_b = find_root(b)
     uf[root_b] = root_a
-    s.add(root_a)
-res = list(s)
-res.sort()
-print(res[0], res[1])
+    groups[root_a] += groups[root_b]
+group = groups[find_root(1)]
+group.sort()
+if group[0] != 1:
+    print(1, group[0])
+else:
+    for i in range(1, len(group)):
+        # 중간에 비어있는겍 존재한다며 
+        if group[i] != group[i-1]+1:
+            print(min(group[0], group[i]-1), max(group[0], group[i]-1))
+            exit()
+    print(1, group[-1] + 1)
