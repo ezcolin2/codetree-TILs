93404d8e75a0b06426d1a5d65f472d919ad31514
ez <105545215+ezcolin2@users.noreply.github.com>
2024-08-30 22:25:49 +0900

    Title: 최소 스패닝 트리, Time: 196ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 240830/최소 스패닝 트리/minimum-spanning-tree.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+uf = [i for i in range(n+1)]
+def find_root(x):
+    if x == uf[x]:
+        return x
+    uf[x] = find_root(uf[x])
+    return uf[x]
+
+res = 0
+for _ in range(m):
+    a, b, w = map(int, input().split())
+    root_a = find_root(a)
+    root_b = find_root(b)
+    # 서로 다르다면
+    if root_a != root_b:
+        uf[root_a] = root_b
+        res += w
+print(res)
