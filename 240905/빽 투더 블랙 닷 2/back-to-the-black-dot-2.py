2da6a16aeee15180fb99bc21fb4bf51a45832890
ez <105545215+ezcolin2@users.noreply.github.com>
2024-09-05 22:47:11 +0900

    Title: 빽 투더 블랙 닷 2, Time: 370ms, Memory: 32MB, Status: Wrong Answer - CodeTree

--- 240905/빽 투더 블랙 닷 2/back-to-the-black-dot-2.py
+++ 240905/빽 투더 블랙 닷 2/back-to-the-black-dot-2.py
@@ -48,11 +48,15 @@ def find_min_distance(n, m, red_a, red_b, graph):
     # 시작점에서 빨간점의 거리를 구하는 것보다 빨간점에서 다른 모든 점에서 구하는 게 효울적
     dist_from_red_a = djikstra(red_a, graph)
     dist_from_red_b = djikstra(red_b, graph)
+
     
     # 최단 거리
     min_distance = INF
     # 시작 점을 하나씩 대입 
     for i in range(1, n+1):
+        # 빨간 점 스킵
+        if i == red_a or i == red_b:
+            continue
         temp_min_distance = get_min_distance(dist_from_red_a, dist_from_red_b, red_a, red_b, i)
         min_distance = min(min_distance, temp_min_distance)
     return min_distance
