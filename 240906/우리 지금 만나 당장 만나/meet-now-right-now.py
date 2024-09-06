26540d48b1f19d1c0e688d6759fac091b7298d39
ez <105545215+ezcolin2@users.noreply.github.com>
2024-09-06 18:33:33 +0900

    Title: 우리 지금 만나 당장 만나, Time: 242ms, Memory: 30MB, Status: Wrong Answer - CodeTree

--- 240906/우리 지금 만나 당장 만나/meet-now-right-now.py
+++ 240906/우리 지금 만나 당장 만나/meet-now-right-now.py
@@ -1,12 +1,12 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-locations = list(map(lambda x:int(x)*10000, input().split()))
+locations = list(map(lambda x:int(x)*100000, input().split()))
 speeds = list(map(int, input().split()))
 
 # 특정 점에 대해서 x초 동안 이동 가능한 위치의 범위를 반환한다.
 def get_location_range(n, location, speed, x):
-    return [max(0, location-speed*x), min(10000000000000, location+speed*x)]
+    return [max(0, location-speed*x), min(100000000000000, location+speed*x)]
 
 # 모두 도착하는 데 걸리는 시간이 x초 이상인 도착점의 겹치는 범위 반환
 # 만약 범위[0] > 범위[1]이라면 해당하는 도착점이 없는 것 
@@ -22,7 +22,7 @@ def get_overrapped_location_range(n, locations, speeds, x):
 
 # 이진 탐색
 def parametric_search(n, locations, speeds):
-    left, right = 0, 10000000000000
+    left, right = 0, 100000000000000
     res_location_range=[]
     min_time = sys.maxsize
     while left<=right:
@@ -37,4 +37,4 @@ def parametric_search(n, locations, speeds):
             left = mid+1
     return min_time
 
-print("{:.4f}".format(parametric_search(n, locations, speeds)/10000))
+print("{:.4f}".format(round(parametric_search(n, locations, speeds)/100000)))
