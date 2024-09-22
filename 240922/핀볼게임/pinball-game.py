71e8f2d1c09463f3422420579f6a6967b7415e78
ez <105545215+ezcolin2@users.noreply.github.com>
2024-09-22 13:01:36 +0900

    Title: 핀볼게임, Time: 139ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240922/핀볼게임/pinball-game.py
+++ 240922/핀볼게임/pinball-game.py
@@ -1,6 +1,5 @@
 n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-
+arr = [[0]*(n+2)] + [[0] + list(map(int, input().split())) + [0] for _ in range(n)] + [[0]*(n+2)]
 # 동서남북 
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
@@ -37,15 +36,15 @@ def change_direction(direction_idx, bar_type):
 
 # 핀볼 게임을 진행하는데 걸리는 시간 반환
 def get_pinball_time(arr, start_x, start_y, start_direction_idx):
-    n = len(arr)
+    n = len(arr)-2
     x, y, direction_idx = start_x, start_y, start_direction_idx
-    # 들어가는 시간과 나가는 시간
-    time = 2 
+    # 나가는 시간
+    time = 1
     # 벗어날 때까지 반복
     while True:
         nx, ny = x + dx[direction_idx], y + dy[direction_idx]
         # 벗어나면 시간 반환
-        if nx < 0 or nx >= n or ny < 0 or ny >= n:
+        if nx < 1 or nx > n or ny < 1 or ny > n:
             return time
         # 바를 만났다면 방향 전환
         if arr[nx][ny] != 0:
@@ -59,20 +58,20 @@ def get_max_time(arr):
     max_time = 0
     n = len(arr)
     # 동
-    for i in range(n):
+    for i in range(1, n+1):
         temp_time = get_pinball_time(arr, i, 0, 0)
         max_time = max(max_time, temp_time)
     # 서
-    for i in range(n):
-        temp_time = get_pinball_time(arr, i, n-1, 1)
+    for i in range(1, n+1):
+        temp_time = get_pinball_time(arr, i, n+1, 1)
         max_time = max(max_time, temp_time)
     # 남
-    for i in range(n):
+    for i in range(1, n+1):
         temp_time = get_pinball_time(arr, 0, i, 2)
         max_time = max(max_time, temp_time)
     # 북
-    for i in range(n):
-        temp_time = get_pinball_time(arr, n-1, i, 3)
+    for i in range(1, n+1):
+        temp_time = get_pinball_time(arr, n+1, i, 3)
         max_time = max(max_time, temp_time)
     return max_time
 print(get_max_time(arr))
