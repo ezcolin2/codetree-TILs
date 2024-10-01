80bde4853b3fe2a1a160c28e3674d26ec66f0fc4
ez <105545215+ezcolin2@users.noreply.github.com>
2024-10-01 14:23:38 +0900

    Title: 사각형 채우기 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241001/사각형 채우기 3/rectangle-fill-3.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+dp = [0]*(n+1)
+dp[0] = 1
+dp[1] = 2
+dp[2] = 7
+dp[3] = 22
+for i in range(4, n+1):
+    dp[i] = dp[i-1]*dp[1] + dp[i-2]*3
+    for j in range(0, i-2):
+        dp[i] += dp[j]*2
+print(dp[n]%1000000007)
