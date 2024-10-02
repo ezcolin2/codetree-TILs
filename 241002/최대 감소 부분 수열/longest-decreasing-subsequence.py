86290cf962822ef0e464a59f10240eac77a02311
ez <105545215+ezcolin2@users.noreply.github.com>
2024-10-02 09:09:46 +0900

    Title: 최대 감소 부분 수열, Time: 147ms, Memory: 25MB, Status: Passed - CodeTree

--- 241002/최대 감소 부분 수열/longest-decreasing-subsequence.py
+++ 241002/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -5,6 +5,5 @@ dp[0] = 1
 for i in range(1, n):
     for j in range(i-1, -1, -1):
         if arr[i] < arr[j]:
-            dp[i] = dp[j] + 1
-            break
+            dp[i] = max(dp[i], dp[j] + 1)
 print(max(dp))
