1ab1030d1d96c5ff543e5df7fe584a6f08b9ae61
ez <105545215+ezcolin2@users.noreply.github.com>
2024-10-03 17:03:13 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 241003/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.py
+++ 241003/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.py
@@ -4,6 +4,7 @@ arr = list(map(int, input().split()))
 asc_dp = [1]*n
 # 내림차순
 desc_dp = [1]*n
+desc_dp_2 = [1]*n
 
 # 오름차순 dp 구하기 
 for i in range(1, n):
@@ -11,13 +12,18 @@ for i in range(1, n):
         if arr[i] > arr[j]:
             asc_dp[i] = max(asc_dp[i], asc_dp[j] + 1)
 
+# 내림차순 dp 구하기 
+for i in range(1, n):
+    for j in range(i):
+        if arr[i] < arr[j]:
+            desc_dp[i] = max(desc_dp[i], desc_dp[j] + 1)
 # 거꾸로 오르마순 dp 구하기
 for i in range(n-2, 0, -1):
     for j in range(n-1, i-1, -1):
         if arr[i] > arr[j]:
-            desc_dp[i] = max(desc_dp[i], desc_dp[j] + 1)
+            desc_dp_2[i] = max(desc_dp_2[i], desc_dp_2[j] + 1)
 
 max_res = 0
-for i in range(n-1):
-    max_res = max(max_res, asc_dp[i] + desc_dp[i+1])
+for i in range(n):
+    max_res = max(max_res, asc_dp[i] + desc_dp_2[i]- 1, asc_dp[i], desc_dp[i])
 print(max_res)
