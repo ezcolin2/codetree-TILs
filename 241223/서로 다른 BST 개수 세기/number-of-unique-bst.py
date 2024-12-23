974cbe741e8e7df5a5e9e4cdf9c99d1aad5ab0a8
ez <105545215+ezcolin2@users.noreply.github.com>
2024-12-23 10:48:21 +0900

    Title: 서로 다른 BST 개수 세기, Time: 85ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241223/서로 다른 BST 개수 세기/number-of-unique-bst.py
@@ -0,0 +1,16 @@
+# n개의 노드로 만들 수 있는 서로 다른 BST의 개수 구하는 함수
+def get_number_of_BST(dp, n):
+    res = 0
+    for i in range(n):
+        res += d[i] * dp[n-i-1]
+    return res
+dp = [0]*20
+dp[1] = 1
+dp[2] = 2
+dp[3] = 5
+
+n = int(input())
+# dp 초기화
+for i in range(4, n+1):
+    dp[i] = get_number_of_BST(dp, i)
+print(dp[n])
