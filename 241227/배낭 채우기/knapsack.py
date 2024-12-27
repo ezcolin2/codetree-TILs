5f7c72fc78d0005d2653edae9b12e801ee31f8a1
ez <105545215+ezcolin2@users.noreply.github.com>
2024-12-27 15:48:54 +0900

    Title: 배낭 채우기, Time: 94ms, Memory: 25MB, Status: Passed - CodeTree

--- 241227/배낭 채우기/knapsack.py
+++ 241227/배낭 채우기/knapsack.py
@@ -9,6 +9,8 @@ for i in range(1, n+1):
     for j in range(1, m+1):
         # 무게를 넘어버리면 스킵
         if j < weight:
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+
             continue
         
         # 1. 이전 무게 고려 
