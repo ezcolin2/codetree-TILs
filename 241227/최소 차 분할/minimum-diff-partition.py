bd616182ce6a241bdd68142698e2461335df94a0
ez <105545215+ezcolin2@users.noreply.github.com>
2024-12-27 17:27:18 +0900

    Title: 최소 차 분할, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241227/최소 차 분할/minimum-diff-partition.py
+++ 241227/최소 차 분할/minimum-diff-partition.py
@@ -1,17 +1,20 @@
+import sys
 n = int(input())
 arr = list(map(int, input().split()))
-# dp[i] : i번째 수까지 고려했을 때 두 그룹 간 차이가 가능한 모든 경우의 수
-# 최소 값은 1000을 초과할 수 없기 때문에 1000이 넘는 경우는 고려하지 않는다.
-# dp[i]는 set으로 구성하여 중복된 값이 없도록 한다.
-# 공간 복잡도 : O(N^2M)
-# 시간 복잡도 : O(N^2M)
-
-# 모든 수를 순회하면서 이전에 나온 모든 값에 대해 덧셈, 뺄셈을 진행한다.
+# 하나의 그룹에서 만들 수 있는 합만 고려한다.
+# 그러면 다른 그룹에서는 그 합을 빼기만 하면 된다.
+# dp[i] : i번째 수까지 고려했을 때 만들 수 있는 합의 경우의 수
 dp = [set() for _ in range(n)]
 dp[0].add(arr[0])
+
 for i in range(1, n):
-    # 이전 값을 모두 순회한다.
-    for value in dp[i-1]:
-        dp[i].add(abs(value-arr[i]))
-        dp[i].add(value+arr[i])
-print(min(dp[-1]))
+    # 이전 합 순회
+    for sum_value in dp[i-1]:
+        # i번째 수를 더하지 않음
+        dp[i].add(sum_value)
+        dp[i].add(sum_value + arr[i])
+total_sum = sum(arr)
+res = sys.maxsize
+for value in dp[-1]:
+    res = min(res, abs(value - (total_sum-value)))
+print(res)
