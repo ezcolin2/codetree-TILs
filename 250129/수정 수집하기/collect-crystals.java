1b8c53dbafe5f3237f5ec736b912b4ceaf7dd469
ez <105545215+ezcolin2@users.noreply.github.com>
2025-01-29 19:56:55 +0900

    Title: 수정 수집하기, Time: 94ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 250129/수정 수집하기/collect-crystals.java
+++ 250129/수정 수집하기/collect-crystals.java
@@ -26,34 +26,53 @@ public class Main {
         // 남은 움직일 수 있는 횟수가 k일 때 수집할 수 있는 수정의 최대 개수 
         int n = crystal.length();
         int[][][] dp = new int[n+1][k+1][2];
-        dp[1][k][0] = 1;
-        for (int i=1; i<=n; i++){
-            // 생성될 수정 위치
-            int crystalLocation = getIndexOfCrystalLocation(crystal.charAt(i-1));
-
-            
+        for (int i=0; i<=n; i++){
             for (int j=0; j<=k; j++){
-                // 수정을 수집하는 경우
-                // 1. 이전에도 남은 횟수가 같았고 같은 방향 (수정을 수집하기 위해 이동하지 않음)
-                // 2. 이전에 남은 횟수가 하나더 있었고 다른 방향 (수정을 수집하기 위해 이동)
-                if (j<k){
-                    dp[i][j][crystalLocation] = Math.max(
-                        dp[i-1][j][crystalLocation]+1, 
-                        dp[i-1][j+1][(crystalLocation+1)%2]+1                
-                    );
-                }
-                else{
-                    dp[i][j][crystalLocation] = dp[i-1][j][crystalLocation]+1;   
-                }
-                
-
-                // 수정을 수집하지 않는 경우
-                // 1. 이전에도 남은 횟수가 같았고 같은 방향 (수정을 수집하지 않기 위해 이동하지 않음)
-                dp[i][j][(crystalLocation+1)%2] = dp[i-1][j][(crystalLocation+1)%2];
-
+                dp[i][j][0] = 0;
+                dp[i][j][1] = 0;
             }
         }
+        
+        for (int i=1; i<=n; i++){
+            // 획득할 수정 위치
+            int crystalLocation = getIndexOfCrystalLocation(crystal.charAt(i-1));
+            for (int j=0; j<=k; j++){
+                for (int p=0; p<2; p++){
+                    // 같은 위치에 수정이 있으면
+                    if (p == crystalLocation){
+                        // 1. 바로 이전에 이동을 한 경우
+                        // 2,. 바로 이전에 이동을 하지 않은 경우
+                        
+                        if (j < k){
+                            dp[i][j][p] = Math.max(dp[i-1][j+1][(p+1)%2], dp[i-1][j][p])+1;
+                        }
+                        // 한 번도 이동하지 않았다면
+                        else{
+                            dp[i][j][p] = dp[i-1][j][p]+1;
+                        }
+                        
+                        
+                    }
+                    // 다른 위치에 수정이 있으면
+                    else{
+                        // 1. 바로 이전에 이동을 한 경우
+                        // 2,. 바로 이전에 이동을 하지 않은 경우
+                        if (j < k){
+                            dp[i][j][p] = Math.max(dp[i-1][j+1][(p+1)%2], dp[i-1][j][p]);
+                        }
+                        // 한 번도 이동하지 않았다면
+                        else{
+                            dp[i][j][p] = dp[i-1][j][p];
+                        }
+                        
+                    }
+                
+                }
+            }
+        }
+
         int res = 0;
+        
         for (int j=0; j<=k; j++){
             res = Math.max(res, Math.max(dp[n][j][0], dp[n][j][1]));
         }
