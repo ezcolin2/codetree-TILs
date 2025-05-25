94222b57346f95f257b0cd077bd5154c2f8f5631
ez <105545215+ezcolin2@users.noreply.github.com>
2025-05-25 18:06:22 +0900

    Title: 게임 스튜디오에서 살아남기, Time: 111ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250525/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.java
@@ -0,0 +1,46 @@
+// dp[i][j][k] : i번째까지 고려했을 때, T가 j번 나오고 B가 연속 k번 나왔을 때, 경우의 수
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][][] dp = new int[n+1][3][3];
+        // 초기화
+        dp[1][0][0] = 1; // G
+        dp[1][1][0] = 1; // T
+        dp[1][0][1] = 1; // B
+
+        // dp 시작
+        for (int i=1; i<n; i++){
+            for (int j=0; j<=2; j++){
+                for (int k=0; k<=2; k++){
+                    // G 받음
+                    // T 그대로
+                    // B 끊김
+                    dp[i+1][j][0] += dp[i][j][k];
+                    // T 받음
+                    // G 그대로
+                    // B 끊김
+                    if (j<2){
+                        dp[i+1][j+1][0] += dp[i][j][k];
+                    }
+                    
+                    // B 받음
+                    // T 그대로
+                    // G 그대로
+                    if (k<2){
+                        dp[i+1][j][k+1] += dp[i][j][k];
+                    }
+                    
+                }
+            }
+        }
+        int res = 0;
+        for (int i=0; i<3; i++){
+            for (int j=0; j<3; j++){
+                res += dp[n][i][j];
+            }
+        }
+        System.out.println(res%1000000007);
+    }
+}
