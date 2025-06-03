2839de1950b56597152250fe496d7e3112b861ce
ez <105545215+ezcolin2@users.noreply.github.com>
2025-06-03 13:47:20 +0900

    Title: 폭탄 해체 작업, Time: 544ms, Memory: 23MB, Status: Passed - Codetree

--- 250603/폭탄 해체 작업/the-bomb-dismantling.java
+++ 250603/폭탄 해체 작업/the-bomb-dismantling.java
@@ -1,48 +1,33 @@
 // dp로 해결
 // dp[i][j] : i번째 폭탄까지 고려했을 때, j개 폭탄을 해제했을 때 점수 최대 값
 import java.util.Scanner;
+import java.util.Queue;
+import java.util.PriorityQueue;
 import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Bomb[] bombs = new Bomb[n+1];
-        bombs[0] = new Bomb(-1, -1);
-        for (int i = 1; i <= n; i++) {
+        Bomb[] bombs = new Bomb[n];
+        Queue<Integer> pq = new PriorityQueue();
+        for (int i = 0; i < n; i++) {
             bombs[i] = new Bomb(sc.nextInt(), sc.nextInt());
         }
         Arrays.sort(bombs);
-
-        int[][] dp = new int[n+1][n+1];
-        // dp 초기화 
-        for (int i=0; i<=n; i++){
-            for (int j=0; j<=n; j++){
-                dp[i][j] = -1;
-            }
-        }
-
-        // 0번째까지 선택한 것은 무조건 0
-        for (int i=0; i<n; i++){
-            dp[0][i] = 0;
-        }
-        for (int i=1; i<=n; i++){
-            for (int j=1; j<=i; j++){
-
-                // i번째를 선택하는 경우
-                // dp[i-1][j-1]이 가능한 경우이면서 시간이 지나지 않은 상태여야 함
-                if (dp[i-1][j-1] != -1 && bombs[i].time >= j){
-                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-1] + bombs[i].score);
-                }
-
-                // i번째를 선택하지 않는 경우
-                if (dp[i-1][j] != -1){
-                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j]);
-                }
-            }
-        }
         int res = 0;
-        for (int i=0; i<=n; i++){
-            res = Math.max(res, dp[n][i]);
+        int bombIdx = n-1;
+        // 뒤에서부터 차례대로
+        for (int time=10001; time>0; time--){
+            // 지금 시간에 해체할 수 있는 모든 폭탄을 pq에 넣는다.
+            while (bombIdx >=0 && bombs[bombIdx].time >= time){
+                pq.add(-bombs[bombIdx--].score);
+            }
+            if (pq.isEmpty()){
+                continue;
+            }
+            // 뽑는다.
+            int score = pq.remove();
+            res += -score;
         }
         System.out.println(res);
     }
@@ -57,7 +42,7 @@ class Bomb implements Comparable<Bomb>{
     @Override
     public int compareTo(Bomb bomb){
         if (this.time == bomb.time){
-            return Integer.compare(bomb.score, this.score);
+            return Integer.compare(this.score, bomb.score);
         }
         return Integer.compare(this.time, bomb.time);
     }
