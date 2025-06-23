f93c7a3cca834ed49f4af3a4758cf6e36656d250
ez <105545215+ezcolin2@users.noreply.github.com>
2025-06-24 07:58:38 +0900

    Title: 동전 거슬러주기, Time: 127ms, Memory: 11MB, Status: Passed - Codetree

--- 250624/동전 거슬러주기/coin-change.java
+++ 250624/동전 거슬러주기/coin-change.java
@@ -8,19 +8,22 @@ public class Main {
         int[] coin = new int[n];
         for (int i = 0; i < n; i++)
             coin[i] = sc.nextInt();
-        int[] dp = new int[Math.max(6, m+1)];
+        int[] dp = new int[100001];
         for (int i=0; i<=m; i++){
             dp[i] = Integer.MAX_VALUE;
         }
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 3;
-        dp[4] = 1;
-        dp[5] = 1;
-        for (int i=6; i<=m; i++){
-            dp[i] = Math.min(dp[i], dp[i-1]+1);
-            dp[i] = Math.min(dp[i], dp[i-4]+1);
-            dp[i] = Math.min(dp[i], dp[i-5]+1);
+        for (int i=0; i<n; i++){
+            dp[coin[i]] = 1;
+        }
+        for (int i=1; i<=m; i++){
+            for (int j=0; j<n; j++){
+                if (i-coin[j] > 0 && dp[i-coin[j]] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i-coin[j]]+1);
+                }
+                
+            }
+            
+
         }
         System.out.println(dp[m] == Integer.MAX_VALUE ? -1 :dp[m]);
     }
